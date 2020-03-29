2272239d81887ef5ddfbe8df8a7c239177fbc666
stephen <1245704060@qq.com>
2020-03-29 23:00:08 +0800

    添加11章两个练习

---
+++ exercise/Ch11Ex02/Ch11Ex02/Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Ch11Ex02
+{
+    public class Animals:CollectionBase
+    {
+        public void Add(Animal newAnimal) => List.Add(newAnimal);
+        public void Remove(Animal oldAnimal) => List.Remove(oldAnimal);
+
+        public Animal this[int animalIndex]
+        {
+            get { return (Animal)List[animalIndex]; }
+            set { List[animalIndex] = value; }
+        }
+    }
+}
