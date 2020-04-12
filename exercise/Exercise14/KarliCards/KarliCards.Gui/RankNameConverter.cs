d9bbc07f357dc51a0239096a5fda83bb2d0f5ae4
nimtecheng <1245704060@qq.com>
2020-04-13 00:02:01 +0800

    关联13章 part1

---
+++ exercise/Exercise14/KarliCards/KarliCards.Gui/RankNameConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KarliCards.Gui
+{
+    [ValueConversion(typeof(Ch13CardLib.Rank),typeof(string))]
+  public  class RankNameConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int source = (int)value;
+            if (source == 1 || source > 10)
+            {
+                switch (source)
+                {
+                    case 1: return "Ace";
+                    case 11: return "Jack";
+                    case 12: return "Queen";
+                    case 13: return "King";
+                    default:
+                        return DependencyProperty.UnsetValue;
+                }
+            }
+            else
+                return source.ToString();
+        }
+    }
+}
